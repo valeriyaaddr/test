863f7c7608cfaf3a18051cff00994b40bc562d7d
Valery <valeriyaaddr@gmail.com>
2020-12-23 16:41:13 +0200

    Added mocks

--- src/app/services/facts.service.ts
+++ src/app/services/facts.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import {Observable, of} from 'rxjs';
+import mockCategories from '../_mocks/categories.json';
+import mockJoke from '../_mocks/joke.json';
+import mockJokes from '../_mocks/jokes.json';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +15,7 @@ export class FactsService {
   constructor(private http: HttpClient) {}
 
   public getCategory(): Observable<any> {
-    return this.http.get(`${environment.apiUrl}/categories`);
+    return of(mockCategories);
   }
 
   public getRandomJoke(params: Record<string, any> = {}): Observable<any> {
@@ -20,13 +23,13 @@ export class FactsService {
     for (const [k, v] of Object.entries(params)) {
       httpParams = httpParams.set(k, v);
     }
-    return this.http.get(`${environment.apiUrl}/random`, {params: httpParams});
+    return of(mockJoke);
   }
 
   public getJokes(search: string): Observable<any> {
     const httpParams = new HttpParams()
       .set('query', search);
-    return this.http.get(`${environment.apiUrl}/search`, {params: httpParams});
+    return of(mockJokes);
   }
 
 }
